142c209c33d1b639008d3d437848b332294d1b08
Jack18621 <21jhill@gshs.org.uk>
2025-09-10 17:44:21 +0100

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-// src/main.tsx
-import { HashRouter } from 'react-router-dom'
-import App from './App'
-
-createRoot(document.getElementById('root')!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-)
